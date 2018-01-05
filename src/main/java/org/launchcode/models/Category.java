a0d798a1dba05c3566de6d12e6af183c9b8466a9
Michelle <mtrusgn0802@att.net>
2018-01-04 18:40:43 -0600

    Final code for all three parts

---
+++ src/main/java/org/launchcode/models/Category.java
@@ -0,0 +1,63 @@
+package org.launchcode.models;
+
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    @Size(min=3, max=15, message="field is empty")
+    private String name;
+
+
+    @OneToMany
+    @JoinColumn(name="category_id")
+
+    private List<Cheese> cheeses = new ArrayList<>();
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public Category(){}
+
+
+
+//getters and setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Cheese> getCheeses() {
+        return cheeses;
+    }
+
+    public void setCheeses(List<Cheese> cheeses) {
+        this.cheeses = cheeses;
+    }
+
+
+
+}
